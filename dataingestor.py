f3150aa72e8c3657d5fadad550660b777c763ac0
Abdullah Natiq <abdullahnatiq8765@gmail.com>
2026-04-07 15:51:46 +0500

    tool use

---
+++ dataingestor.py
@@ -0,0 +1,25 @@
+import config
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_chroma import Chroma
+
+def buildMarketIndex():
+    print(f"Loading: {config.pdfpath}")
+    loader = PyPDFLoader(config.pdfpath)
+    pages = loader.load()
+
+    splitter = RecursiveCharacterTextSplitter(chunk_size = 500, chunk_overlap = 50)
+    chunks = splitter.split_documents(pages)
+
+    Embeddings = HuggingFaceEmbeddings(model_name = config.embeddingModel)
+
+    Chroma.from_documents(
+        documents = chunks,
+        embedding = Embeddings,
+        persist_directory = config.chromaDIR
+    )
+    print("Success: Vector database created in" + config.chromaDIR)
+
+if __name__ == "__main__":
+    buildMarketIndex()
